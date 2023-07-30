660d614a81a793c490050c876ec6a47af62cf401
SofiCaviglia <140869356+SofiCaviglia@users.noreply.github.com>
2023-07-30 20:10:31 -0300

    Commit inicial

---
+++ Trabajo-matematica/efecto-scroll.js
@@ -0,0 +1,89 @@
+window.sr = ScrollReveal();
+  sr.reveal('.textos-header', {
+    duration: 2500,
+    distance: '400px',
+    origin: 'top',
+    
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.titulo-logo-celular', {
+    duration: 3000,
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.valor-no-posicional', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'left'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.posicion', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'ring'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.valor-posicional', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'left'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.valor-posicional-2', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'left'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.posicion-2', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'ring'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.comparacion-sistemas', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'left'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.iframe-valor', {
+    delay: 0,
+    duration: 3000,
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.juego', {
+    delay: 0,
+    duration: 3000,
+  });
+
+
+  window.sr = ScrollReveal();
+  sr.reveal('.red-social', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'left'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.horarios', {
+    delay: 0,
+    duration: 1000,
+    distance: '200px',
+    origin: 'ring'
+  });
